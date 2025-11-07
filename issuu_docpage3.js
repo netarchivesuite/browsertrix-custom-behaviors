465528390fc16d64ebb9ad5769460e26b6f56dda
thsm-kb <96050897+thsm-kb@users.noreply.github.com>
2025-11-07 13:34:34 +0100

    Update and rename issuu_docfile3.js to issuu_docpage3.js

--- issuu_docpage3.js
+++ issuu_docpage3.js
@@ -12,18 +12,13 @@ class QueueIssuuIframe {
   static runInIframes = true;
 
   async* run(ctx) {
-    const { waitUntilNode, addLink } = ctx.Lib;
-
     // wait for the iframe by XPath
     const iframe = await ctx.Lib.waitUntil(() => document.querySelector("#DocPageReaderIframe"));
     await new Promise(r => setTimeout(r, 1000));
-    ctx.log({ msg: "iframe src", iframe?.getAttribute("src") });
-    //const url = iframe?.src || iframe?.getAttribute("src");
-    //ctx.log({ msg: "iframe src", url });
-
-    let url = document.querySelector('#DocPageReaderIframe')?.src;
+    
+    let url = iframe.src;
     ctx.log({ msg: "iframe src", url });
-
-    if (url) await addLink(url);
+    await ctx.Lib.addLink(url);
+    
   }
 }
